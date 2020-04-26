2fe8eba72ae56b32134d4b9d6682b4de24a7d3b9
Hellowalls1 <thesingingbutcher@gmail.com>
2020-04-26 16:48:26 -0500

    chapter 9

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -10,6 +10,8 @@ import { EmployeeProvider } from './employees/EmployeeProvider'
 import "./employees/Employees.css"
 import { CustomerProvider } from './customers/CustomerProvider'
 import { CustomerOrderProvider} from "./customers/CustomerOrderProvider"
+import { Button, Modal, ModalBody, ModalHeader } from "reactstrap"
+import OrderList from "./customers/OrderList"
 
 export default () => (
     <>
@@ -31,11 +33,20 @@ export default () => (
             <ProductProvider>
               <ProductTypeProvider>
                 <EmployeeProvider>
+                  <OrderList />
                  <h2>Locations</h2>
                 <LocationList />
                   <h2>Products</h2>
                 <ProductList />
                 <EmployeeList />
+                <Modal isOpen={modal} toggle={toggle}>
+                  <ModalHeader toggle={toggle}>
+                      Order:
+                      </ModalHeader>
+                       <ModalBody>
+                      <OrderList toggle={toggle} />
+                      </ModalBody>
+                      </Modal>
               </EmployeeProvider>
             </ProductTypeProvider>
          </ProductProvider>
---
+++ src/components/customers/OrderList.js
@@ -0,0 +1,44 @@
+import React, { useContext, useState} from "react"
+import { CustomerOrderContext } from "./CustomerOrderProvider"
+import { CustomerContext } from "CustomerProvider.js"
+import { ProductContext } from "../products/ProductProvider"
+ import { Button, Modal, ModalBody, ModalHeader } from "reactstrap"
+
+
+export default () => {
+
+    const { customers } = useContext(CustomerContext)
+    const { products } = useContext(ProductContext)
+    const { orders } = useContext (CustomerOrderContext)
+
+    const [modal, setModal] = useState(false)
+    const toggle = () => setModal(!modal)
+
+    return (
+        <>
+     <Button onClick={() => {
+         //check if the user is authenticated
+         const userId = localStorage.getItem("kandy_customer") //where does kandy customer come from
+        if (userId){
+            //if the user is authenticated, show the animal form
+            toggle() //function that sets the modal state to the opposite on line 20
+        }
+    }}>My Order</Button>
+     <div className="orders">
+    {
+        orders.map(ord => {
+            const matchingCandyInfo = products.find(p => p.id === ord.productId)
+        
+        return <Order key={ord.id}
+
+        candy={matchingCandyInfo}
+        
+        order={ord} />
+    })
+}
+    </div>    
+    </>
+)
+}
+    
+      
